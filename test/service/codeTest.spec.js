b5770ae4e181c19906b9643cc2bf12b37e9d31dd
장재훈 <codelife@11st.co.kr>
2018-03-23 18:45:00 +0900

    Merge branch 'master' of https://github.com/codelifehoon/suggues-fr
    # Conflicts:
    #	appNode.js
    #	com/util/CookieManager.js
    #	routes/RouteConfig.js
    #	routes/sv/content.js
    #	service/UserProcess.js

--- test/service/codeTest.spec.js
+++ test/service/codeTest.spec.js
@@ -1,7 +1,8 @@
 'use strict'
 import request from 'supertest';
-import { expect } from 'chai';
-import userProcess from '~/service/UserProcess'
+import {stateFromMarkdown} from 'draft-js-import-markdown';
+import {stateToHTML} from 'draft-js-export-html';
+
 import fetch from 'node-fetch';
 
 
@@ -16,8 +17,23 @@ describe('Code Test', () => {
 
     });
 
+    it ('markDown to html',()=>{
+
+        const markDown = '# this **markd**own editer..&nbsp' ;
+
+
+        let contentState = stateFromMarkdown(markDown);
+        let html = stateToHTML(contentState);
+
+        console.log(html);
+
+
+
+    });
+
 
 });
+
 
 
 
