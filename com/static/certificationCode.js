e38960ff947e4ecb46a9a8ff025d2f02f7f298b4
장재훈 <codelife@11st.co.kr>
2018-07-26 14:45:32 +0900

    add

---
+++ com/static/certificationCode.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+export const FACEBOOK_CLIENTID = '1747638782217034';
+export const FACEBOOK_CLIENTSECRET  = 'bdfc6ed55ae6417a5cb6a68589cfde00';
+
+export const NAVER_CLIENTID = 'WrgvVU7JGQQs0B7WSTqy';
+export const NAVER_CLIENTSECRET  = 'yT9jrriB9N';
+
+export const GOOGLE_CLIENTID = '130195882344-8hvernj7vv07l1nt5v321lpc707b1c66.apps.googleusercontent.com';
+export const GOOGLE_CLIENTSECRET  = 'bjH-jE-_65wwve8dCbvt3Ake';
